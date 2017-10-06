7cd244c4e6c4bd256885c43a0416cbac47f0e724
Ari Lacenski <ari@weconnectrecovery.com>
2017-10-05 20:57:23 -0700

    Initial commit.

---
+++ app/src/main/java/net/tensory/rxjavatalk/models/Battle.java
@@ -0,0 +1,4 @@
+package net.tensory.rxjavatalk.models;
+
+public class Battle {
+}
---
+++ app/src/main/java/net/tensory/rxjavatalk/providers/MergedBattleProvider.java
@@ -0,0 +1,17 @@
+package net.tensory.rxjavatalk.providers;
+
+import net.tensory.rxjavatalk.models.Battle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.Observable;
+
+public class MergedBattleProvider {
+
+//    public Observable<Battle> getBattles(OsObservable<Battle> battlesObservable) {
+//        battlesObservable.
+//        return Observable.merge(battlesObservable, battlesObservable);
+//    }
+}
